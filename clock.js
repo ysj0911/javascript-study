ee768711d9bd8284ce6ec6c5590f1c3409e2d759
ysj0911 <whgdk0911.gmail.com>
2023-01-14 22:19:23 +0900

    6

---
+++ clock.js
@@ -0,0 +1,13 @@
+const clock = document.querySelector("h2#clock")
+
+function getCloock(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+    
+}
+
+getCloock()
+setInterval(getCloock, 1000); 
