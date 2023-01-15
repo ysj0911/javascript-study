5f4f621bf90176568fb32779a4ba0415ddf1151c
ysj0911 <whgdk0911@gmail.com>
2023-01-16 02:26:30 +0900

    7

--- weather.js
+++ weather.js
@@ -1,7 +1,10 @@
+const API_KEY = "24a1cef063ae56a4197dc02320d6f2cc"
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
+    const lon = position.coords.longitude;
     console.log(position);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
 }
 function onGeoError(){
     alert("Can't find you. No weather for you.")
