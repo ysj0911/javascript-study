2d6a6f81616500d01af16a76cb832d9f26006308
ysj0911 <whgdk0911@gmail.com>
2023-01-16 10:42:11 +0900

    8

--- weather.js
+++ weather.js
@@ -5,9 +5,17 @@ function onGeoOk(position){
     const lon = position.coords.longitude;
     console.log(position);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name; 
+        weather.innerText = data.weather[0].main;
+    });
 }
 function onGeoError(){
     alert("Can't find you. No weather for you.")
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError); 
